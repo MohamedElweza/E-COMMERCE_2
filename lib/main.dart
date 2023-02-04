70aaca2cfe519cf55f27ba8edaa8eae45939e8ca
Mohamed Elweza <moahmed9899@gmail.com>
2023-02-05 01:30:46 +0200

    some updates

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.orange,
       ),
-      home:  TabsScreen(),
+      home:  const SplashScreen(),
     );
   }
 }
